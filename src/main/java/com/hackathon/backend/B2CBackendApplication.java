af5be837601021a09e1e0c778b7cef5b4f6aeca5
pasandevin <jayawardene.pasandevin@gmail.com>
2023-03-27 17:18:39 +0530

    Add initial spring project

---
+++ src/main/java/com/hackathon/backend/B2CBackendApplication.java
@@ -0,0 +1,11 @@
+package com.hackathon.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class B2CBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(B2CBackendApplication.class, args);
+    }
+}
