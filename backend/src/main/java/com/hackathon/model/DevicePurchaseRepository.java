836ad8cdbbcb1aac1a0c75c258e96d3cfd0b99ba
pasandevin <jayawardene.pasandevin@gmail.com>
2023-03-28 18:34:12 +0530

    Add JpaRepository classes

---
+++ backend/src/main/java/com/hackathon/model/DevicePurchaseRepository.java
@@ -0,0 +1,8 @@
+package com.hackathon.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DevicePurchaseRepository extends JpaRepository<DevicePurchase, DevicePurchaseID> {
+}
---
+++ backend/src/main/java/com/hackathon/model/DeviceRepository.java
@@ -0,0 +1,8 @@
+package com.hackathon.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DeviceRepository extends JpaRepository<Device, String> {
+}
