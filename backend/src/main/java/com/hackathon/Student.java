21e75b1a48a1a517f6f149ab34c1374e934d3ecf
pasandevin <jayawardene.pasandevin@gmail.com>
2023-03-28 11:44:31 +0530

    Create simple test api

---
+++ backend/src/main/java/com/hackathon/Student.java
@@ -0,0 +1,18 @@
+package com.hackathon;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    public Integer id;
+    public String name;
+    public int age;
+    public String city;
+
+}
