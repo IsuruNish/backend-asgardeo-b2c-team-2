9c07b1e60160476a13ab4958c5455b2d91b8d1fe
pasandevin <jayawardene.pasandevin@gmail.com>
2023-03-28 16:55:06 +0530

    Add data model for services

---
+++ backend/src/main/java/com/hackathon/model/Service.java
@@ -0,0 +1,44 @@
+package com.hackathon.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Service {
+    @Id
+    @Column(name = "id", nullable = false, updatable = false)
+    private String id;
+    @Column(columnDefinition = "TEXT")
+    private String type;
+    @Column(columnDefinition = "TEXT")
+    private String name;
+    @Column(columnDefinition = "TEXT")
+    private Integer price;
+
+    public Service() {
+    }
+
+    public Service(String id, String type, String name, Integer price) {
+        this.id = id;
+        this.type = type;
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+}
