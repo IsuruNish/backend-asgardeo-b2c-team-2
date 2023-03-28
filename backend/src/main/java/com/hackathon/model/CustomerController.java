c53df5eea9347b85f74728b4db5dc8c26e77e652
pasandevin <jayawardene.pasandevin@gmail.com>
2023-03-28 18:34:34 +0530

    Add CustomerController

---
+++ backend/src/main/java/com/hackathon/model/CustomerController.java
@@ -0,0 +1,19 @@
+package com.hackathon.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "customer")
+public class CustomerController {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+    @PostMapping("addnewcustomer")
+    public void addNewCustomer(@RequestBody Customer customerData) {
+        customerRepository.save(customerData);
+    }
+}
